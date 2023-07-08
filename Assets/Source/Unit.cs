12f0fb8452fbf8e72f4de96fd51a4d501f52affd
butterdawgg <butterdawgg06@gmail.com>
2023-07-08 15:06:11 +0300

    units added

--- Assets/Source/Unit.cs
+++ Assets/Source/Unit.cs
@@ -7,34 +7,79 @@ using Unity.Mathematics;
 public class Unit : MonoBehaviour
 {
     [SerializeField] private SplineContainer sc;
+    [SerializeField] private float speed;
+    [SerializeField] private float minX;
+    [SerializeField] private float minZ;
+    [SerializeField] private float maxX;
+    [SerializeField] private float maxZ;
 
-    [SerializeField] [Range(0f, 1f)] private float _time;
-
-    [SerializeField] private float offset;
+    private Rigidbody rb;
 
     private Vector3 position;
-    private Vector3 tangent;
+    private Vector3 forwardVector;
     private Vector3 upVector;
+    private Vector3 rightVector;
 
+    private float offsetX;
+    private float offsetZ;
+
+    private bool levelStarted = false;
 
     private void Awake()
     {
-        Evaluate(0f);
+
     }
 
     private void Update()
     {
-        Evaluate(_time);
+        if (levelStarted)
+            return;
 
-        transform.position = position + (Vector3.Cross(tangent, upVector).normalized * offset);
+        if (Input.GetKeyDown(KeyCode.Space))
+            OnLevelStart();
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, minX, maxX), transform.position.y, Mathf.Clamp(transform.position.z, minZ, maxZ));
+
+        offsetX = transform.position.z;
+        offsetZ = transform.position.x;
+    }
+
+    private void OnLevelStart()
+    {
+        StartCoroutine(OnLevelStartCoroutine());
+    }
+
+    private IEnumerator OnLevelStartCoroutine()
+    {
+        levelStarted = true;
+
+        float time = (offsetZ / speed) / (sc.CalculateLength(0) / speed);
+        while (time < 1f)
+        {
+            Evaluate(time);
+
+            transform.position = position + (rightVector * offsetX);
+
+            time += Time.deltaTime / (sc.CalculateLength(0) / speed);
+            yield return null;
+        }
+
+        yield return null;
+
+        levelStarted = false;
     }
 
     private void Evaluate(float time)
     {
-        sc.Evaluate(time, out float3 positionTemp, out float3 tangentTemp, out float3 upVectorTemp);
+        sc.Evaluate(time, out float3 pos, out float3 tangent, out float3 up);
 
-        position = positionTemp;
-        tangent = tangentTemp;
-        upVector = upVectorTemp;
+        position = pos;
+        forwardVector = tangent;
+        upVector = up;
+        rightVector = Vector3.Cross(forwardVector, upVector);
+
+        forwardVector.Normalize();
+        upVector.Normalize();
+        rightVector.Normalize();
     }
 }
